0012a943c6bb5537c768cfd5b4fe4058c8cc59c9
SeongGwangJu <seonggwangdev@gmail.com>
2023-08-02 17:01:39 +0900

    마지막교시 전까지

---
+++ JS/array.js
@@ -0,0 +1,92 @@
+window.onload = () => {
+    console.log("userList");
+    console.log(userList);
+
+    console.log("userList[0].username");
+    console.log(userList[0].username);
+    
+    //출력이 어떻게 되는지 볼 것
+    console.log("`username: userList[0].username`");
+    console.log(`username: userList[0].username`);
+    console.log("`username: ${userList[0].username}`");
+    console.log(`username: ${userList[0].username}`); //${}안에는 변수로 인식함
+    //근데 왜 백쿼터를 써야하는데?
+    
+    loadUserList();
+
+    const numbers = [1,2,3,4,5];
+    const fx = (n) => {
+        return n * 2;
+    }
+    const numbers2 = numbers.map(fx);
+    //넘버스 2랑 3이랑 똑같다.
+    const numbers3 = numbers.map((n) => {
+        return n * 2;
+    });
+
+    console.log("`numbers: ${numbers}`");
+    console.log(`numbers: ${numbers}`);
+
+    console.log("numbers.join("-")");
+    console.log(numbers.join("-"));
+
+    console.log(`numbers2: ${numbers2}`);
+    console.log(`numbers3: ${numbers3}`);
+
+
+
+}
+
+const userList = new Array();
+userList.push({username: "aaa", password: "1111" });
+ //*이 자체가 객체다. { key : value }형태
+userList.push({username: "bbb", password: "2222" });
+userList.push({username: "ccc", password: "3333" });
+userList.push({username: "ddd", password: "4444" });
+userList.push({username: "eee", password: "5555" });
+userList.push({username: "fff", password: "6666" });
+
+const loadUserList = () => {
+    const userListTbody = document.querySelector(".user-list");
+
+    //innerHTML : HTML안에 밑의 내용을 통째로 넣을 수 있다.
+    // '${}' 형태 ->변수를 넣을 수 있다.
+    for(let i = 0; i<userList.length; i++) {
+        //반복돌면서 문자열이 추가된다
+        userListTbody.innerHTML += ` 
+        <tr>
+            <td>${userList[i].username}</td>
+            <td>${userList[i].password}</td>
+        </tr>
+        `;
+    }
+
+    //foreach
+    //userList에서 
+
+    /* const fx = (user) => {
+        return `
+            <tr>
+                <td>${user.username}</td>
+                <td>${user.password}</td>
+            </tr>
+        `
+    } */
+                //파란괄호 시작부터 위의 fx를 넣은거랑 똑같은 결과임.
+                
+    const mapTest = userList.map((user) => {
+        return `
+            <tr>
+                <td>${user.username}</td>
+                <td>${user.password}</td>
+            </tr>
+        `
+    });
+    
+    console.log("mapTest");   
+    console.log(mapTest);
+
+    console.log("mapTest.join()");   
+    console.log(mapTest.join(""));
+
+};
---
+++ JS/array2.js
@@ -0,0 +1,83 @@
+window.onload = () => {
+    loadBookList();
+    loadBookList2();
+    loadBookList3();
+};
+
+const bookList = new Array();
+
+bookList.push({
+    bookName: "자바의정석",
+    author: "남궁석",
+    publication: "모름ㅋ"
+});
+bookList.push({
+    bookName: "자바스크립트의정석",
+    author: "김준일",
+    publication: "코리아it"
+});
+bookList.push({
+    bookName: "자바의허접",
+    author: "주성광",
+    publication: "우리집"
+});
+bookList.push({
+    bookName: "DoItHTML",
+    author: "고경희",
+    publication: "이지스 퍼블리싱"
+});
+bookList.push({
+    bookName: "수학의정석",
+    author: "홍성대",
+    publication: "성지"
+});
+
+const loadBookList = () => {
+    const bookListOl = document.querySelector(".book-list")
+    bookListOl.innerHTML = ''; //초기화 후 대입하면 기존 클래스 아래를 '대체'함. 
+    for(let i =0; i<bookList.length; i++) {
+        bookListOl.innerHTML += `
+        <li>
+            <span>${bookList[i].bookName}</span>
+            <span>${bookList[i].author}</span>
+            <span>${bookList[i].publication}</span>
+        </li>
+        `;
+    }
+};
+
+//강사님풀이(2)
+const loadBookList2 = () => {
+    const bookListOl2 = document.querySelector(".book-list2")
+
+    bookListOl2.innerHTML = bookList.map(book => {
+        return `
+            <li>
+                <span>${book.bookName}</span> 
+                <span>${book.author}</span>
+                <span>${book.publication}</span>
+            </li>
+        `;
+    }).join("")
+}
+
+//강사님 풀이를 풀어서 이해해보자(3)
+const loadBookList3 = () => {
+    const bookListOl3 = document.querySelector(".book-list3")
+
+    //위의 for문을 아래의 foreach로 바꿔보자
+    const fx = (book) => {
+        return `
+            <li>
+                <span>${book.bookName}</span> 
+                <span>${book.author}</span>
+                <span>${book.publication}</span>
+            </li>
+        `
+    }
+
+    const bookTest = bookList.map(fx);
+    
+    bookListOl3.innerHTML = bookTest.join("")
+    
+}
